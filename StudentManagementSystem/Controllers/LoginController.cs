662724af10382e330ea8f6176b288dbf716ac325
Mark Josmil Degamo <MDegamo01@lear.com>
2022-08-16 11:18:25 +0800

    commit

---
+++ StudentManagementSystem/Controllers/LoginController.cs
@@ -0,0 +1,52 @@
+﻿using StudentManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentManagementSystem.Controllers
+{
+    public class LoginController : Controller
+    {
+        Database1Entities db = new Database1Entities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(user userObj)
+        {
+            if (ModelState.IsValid)
+            {
+                using (Database1Entities db = new Database1Entities())
+                {
+                    var obj = db.users.Where(a => a.username.Equals(userObj.username) && a.password.Equals(userObj.password)).FirstOrDefault();
+
+                    if (obj != null)
+                    {
+                        Session["UserID"] = obj.Id.ToString();
+                        Session["UserName"] = obj.username.ToString();
+                        Session["Password"] = obj.password.ToString();
+                        return RedirectToAction("Index", "Home");
+                    } else
+                    {
+                        ModelState.AddModelError("", "The username/password is incorrect");
+                    }
+                }
+            }            
+
+            return View(userObj);
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
---
+++ StudentManagementSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StudentManagementSystem.Startup))]
+namespace StudentManagementSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
